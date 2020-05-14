542841c45b9bf3091ac17a641d7d56ff4ff0d50d
brieucdlf <github.e24q4@simplelogin.fr>
2020-05-14 23:08:40 +0200

    :art: Update polybar

--- scripts/polybar.sh
+++ scripts/polybar.sh
@@ -1,12 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-# Terminate already running bar instances
+# Terminate already running polybar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Get all the monitor connected to display polybar
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-  MONITOR=$m polybar --reload main & 
-done
+# Launch polybar
+polybar main &
+polybar external &
+
+# Multi monitor test
+# for m in $(polybar --list-monitors | cut -d":" -f1); do
+#   MONITOR=$m polybar --reload main & 
+# done
