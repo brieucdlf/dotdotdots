d8c0632b1aca9d9866d8c374a0c24e6f6743d42b
brieucdlf <github.e24q4@simplelogin.fr>
2024-08-01 11:13:20 +0200

    build: add backoffice as alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -61,6 +61,7 @@ alias monito="kubectl -n monitoring get pods"
 # works only
 alias front="cd $HOME/Repos/bloomflow/sflow-flamingo"
 alias api="cd $HOME/Repos/bloomflow/api-platform"
+alias bo="cd $HOME/Repos/bloomflow/back-office"
 alias infra="cd $HOME/Repos/bloomflow/sflow-infra"
 alias deploy="cd $HOME/Repos/bloomflow/sflow-deploy"
 alias e2e="cd $HOME/Repos/bloomflow/automated-e2e"
