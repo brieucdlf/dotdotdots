7809939e892c77d1878fbe9cfcea05618f3f9e84
brieucdlf <github.e24q4@simplelogin.fr>
2026-05-12 19:46:04 +0200

    feat(nvim): apply Nurburgreen theme via catppuccin overrides

--- .config/omarchy/themes/nurburgreen/neovim.lua
+++ .config/omarchy/themes/nurburgreen/neovim.lua
@@ -1,16 +1,13 @@
 return {
   {
-    "sainnhe/everforest",
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
-    config = function()
-      vim.g.everforest_background = "hard"
-      vim.g.everforest_better_performance = 1
-    end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "everforest",
+      colorscheme = "catppuccin",
     },
   },
 }
